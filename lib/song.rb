35ec7ea2aa3c0f49a27a916c6ca8c24268edc570
Celina Jackson <celinanjackson@gmail.com>
2020-07-19 19:15:54 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -6,8 +6,8 @@ class Song
     @name = name
     @artist = artist
     @genre = genre
-    @artist.songs << self 
-    @artists.genres << genre 
+    @artist.songs << self
+    @artists.genres << genre
     @genre.songs << self
     @genre.artists << artist 
     @@all << self
