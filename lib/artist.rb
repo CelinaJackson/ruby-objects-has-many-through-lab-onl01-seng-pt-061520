e404b1d7b37daff43e8794bd0fdd337e52e38dcf
Celina Jackson <celinanjackson@gmail.com>
2020-07-19 19:13:35 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,10 +1,12 @@
 def Artist
-  attr_accessor :name, :songs, :genre
+  attr_accessor :name, :songs, :genres
 
   @@all = []
 
 def initialize(name)
   @name = name
+  @songs = []
+  @genres = []
   @@all << self
 end
 
