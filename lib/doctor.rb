4b5a2105f070c45c9c9367b87ffa34452dd39761
Celina Jackson <celinanjackson@gmail.com>
2020-07-19 18:41:34 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -12,9 +12,9 @@ class Doctor
   end
 
   def appointments
-    Appointments.all.select {|appointment| appointment.doctor == self}
+    Appointment.all.select {|appointment| appointment.doctor == self}
   end
-  
+
   def new_appointment(patient, date)
     Appointment.new(patient, date, self)
   end
